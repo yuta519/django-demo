42ee3435bd94e4be5ce2a13244842cade606d245
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-08-06 23:28:20 +0900

    change dir

--- apps/api/views/user_views.py
+++ apps/api/views/user_views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
