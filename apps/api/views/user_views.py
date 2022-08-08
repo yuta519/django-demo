4c39a8261d244f498ddc001f5b620d9b6920d196
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-08-08 10:51:55 +0900

    add post but not work well

--- apps/api/views/user_views.py
+++ apps/api/views/user_views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views import View
-from django.shortcuts import render
-from rest_framework.parsers import JSONParser
 
 from apps.api.models.user_models import User
 from apps.api.serializers import UserSerializer
@@ -12,3 +9,7 @@ class UsersView(View):
     def get(self, request):
         serializer = UserSerializer(User.objects.all(), many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    # need to improve CSRF
+    def post(self, request, *args, **kawrgs):
+        print("POST")
