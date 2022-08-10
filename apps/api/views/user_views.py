9df0d1c805c9d786b368bcd50e59e6242fc1f525
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-08-11 05:10:02 +0900

    make userview post method work well

--- apps/api/views/user_views.py
+++ apps/api/views/user_views.py
@@ -1,18 +1,21 @@
-from django.http import JsonResponse
-from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.api.models.user_models import User
 from apps.api.serializers import UserSerializer
 
 
-class UsersView(View):
+class UsersView(APIView):
     def get(self, request):
         serializer = UserSerializer(User.objects.all(), many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    # need to improve CSRF
     @csrf_exempt
-    def post(self, request, *args, **kawrgs):
-        print("POST")
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
