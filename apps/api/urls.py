004175900890493f5a3a663cc0a9617f6ceb5a68
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-08-11 05:00:47 +0900

    add apss users.py for routing

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -13,15 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
-from apps.api import urls
+from apps.api.views.user_views import UsersView
 
 
 urlpatterns = [
-    path('', include(urls)),
-    path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("api/users", UsersView.as_view()),
 ]
--- apps/api/views/user_views.py
+++ apps/api/views/user_views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets
 
 from apps.api.models.user_models import User
 from apps.api.serializers import UserSerializer
@@ -11,5 +13,6 @@ class UsersView(View):
         return JsonResponse(serializer.data, safe=False)
 
     # need to improve CSRF
+    @csrf_exempt
     def post(self, request, *args, **kawrgs):
         print("POST")
