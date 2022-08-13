46583aebd3fe484de9a3f5d3e96827c0b8af9fe4
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-08-13 23:48:30 +0900

    erase comments

--- apps/api/models/user_models.py
+++ apps/api/models/user_models.py
@@ -2,8 +2,6 @@ import uuid
 
 from django.db import models
 
-# Create your models here.
-
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
